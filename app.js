ab1a8a702cc9dd9f71bcede4f163f81178a43c53
Sunday Mark <166103886+sundaymark2020@users.noreply.github.com>
2026-09-14 20:52:22 +0100

    create the models

--- app.js
+++ app.js
@@ -1,53 +1,138 @@
+
+```js
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '8.8.4.4']);
-const express = require('express');
 
+const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+
 require('dotenv').config();
 
 const app = express();
-const morgan = require('morgan');
+
+
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.static('public'));
+
+
+// Blog Schema
+const blogSchema = new mongoose.Schema({
+    title: String,
+    snippet: String,
+    body: String
+});
+
+
+// Blog Model
+const Blog = mongoose.model('Blog', blogSchema);
+
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
+
         console.log('Connected to MongoDB');
 
         app.listen(3000, function() {
             console.log('Server is running on port 3000');
         });
+
     })
     .catch((error) => {
+
         console.log('MongoDB connection error:', error);
+
     });
 
 
-
-app.use(morgan('dev'));
-app.use(express.static('public')); // Serve static files from the 'public' directory
-app.listen(3000,function(){
-    console.log('Server is running on port 3000');
-});
-
-app.get('/', function(req, res){
+// Home page
+app.get('/', function(req, res) {
 
     const blogs = [
-        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' }
+        {
+            title: 'Yoshi finds eggs',
+            snippet: 'Lorem ipsum dolor sit amet consectetur'
+        },
+        {
+            title: 'Mario finds stars',
+            snippet: 'Lorem ipsum dolor sit amet consectetur'
+        },
+        {
+            title: 'How to defeat bowser',
+            snippet: 'Lorem ipsum dolor sit amet consectetur'
+        }
     ];
-    res.render('index', { title: 'Home Page', blockNinja: 'Ninja Block', netNinja: 'Ninja Net', blogs: blogs });
+
+    res.render('index', {
+        title: 'Home Page',
+        blockNinja: 'Ninja Block',
+        netNinja: 'Ninja Net',
+        blogs: blogs
+    });
+
 });
 
-app.get('/about', function(req, res){
-    res.render('about', { title: 'About Page', blockNinja: 'Ninja Block', netNinja: 'Ninja Net' });
+
+// About page
+app.get('/about', function(req, res) {
+
+    res.render('about', {
+        title: 'About Page',
+        blockNinja: 'Ninja Block',
+        netNinja: 'Ninja Net'
+    });
+
 });
 
-app.get('/blogs/create', function(req, res){
-    res.render('create', { title: 'Create Page', blockNinja: 'Ninja Block', netNinja: 'Ninja Net' });
+
+// Create blog page
+app.get('/blogs/create', function(req, res) {
+
+    res.render('create', {
+        title: 'Create Page',
+        blockNinja: 'Ninja Block',
+        netNinja: 'Ninja Net'
+    });
+
 });
 
-app.use(function(req, res){
-    res.status(404).render('404', { title: 'Page Not Found' });
-}); 
+
+// Blog route
+app.get('/blog', function(req, res) {
+
+    const blog = new Blog({
+        title: 'Yoshi finds eggs',
+        snippet: 'This is a new blogpost',
+        body: 'This is the full content of the blog post.'
+    });
+
+    blog.save()
+        .then(function(result) {
+
+            res.send(result);
+
+        })
+        .catch(function(err) {
+
+            console.log(err);
+
+        });
+
+});
+
+
+// 404 page
+app.use(function(req, res) {
+
+    res.status(404).render('404', {
+        title: 'Page Not Found'
+    });
+
+});
+```
